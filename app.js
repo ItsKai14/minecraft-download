a965c94ad7cbdbf019690bfdfd56467f346ece39
ItsKai14 <85945740+ItsKai14@users.noreply.github.com>
2023-11-03 18:38:23 +0530

    UPLOADED PAGE FILES

---
+++ app.js
@@ -0,0 +1,9 @@
+angular.module('myApp', [])
+.controller('myCtrl', function ($scope) {
+  $scope.clickCount = localStorage.getItem('clickCount') || 0;
+
+  $scope.incrementClickCount = function () {
+    $scope.clickCount--;
+    localStorage.setItem('clickCount', $scope.clickCount);
+  };
+});
